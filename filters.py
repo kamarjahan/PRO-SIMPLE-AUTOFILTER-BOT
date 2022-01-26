a0bf3cad5c4d55b7a3af02c87fb09245f3e50c0d
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-01-26 19:22:00 +0530

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# @trojanzhex
+# @devourdevils
 
 
 import re
@@ -177,7 +177,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("HELP", callback_data="help_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")]
+                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/septemberfilms")]
             ])
 
             await query.message.edit_text(
@@ -192,7 +192,8 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="start_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")]
+                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://t.me/devourdevils")]
+                [InlineKeyboardButton("⭕️ SUPPORT 2 ⭕️", url="https://t.me/TEAM_NARUTO_GRUOP")]
             ])
 
             await query.message.edit_text(
@@ -207,7 +208,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/TroJanzHEX/Auto-Filter-Bot-V2")]
+                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/kamarjahan/PRO-SIMPLE-AUTOFILTER-BOT")]
             ])
 
             await query.message.edit_text(
