2336eac1563199667b319e509d776175450d8ae5
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-01-26 18:02:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/kamarjahan/PRO-SIMPLE-AUTOFILTER-BOT /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
